8cd02c07f2b8219aed16a100b600c594381d9680
gillerick <ogayogill95@gmail.com>
2021-01-24 14:32:22 +0300

    Recursion-based solution

---
+++ Word Bank.py
@@ -0,0 +1,15 @@
+def canConstruct(target, wordBank):
+    if target == "":
+        return True
+    for word in wordBank:
+        # Check if substring is index of word
+        try:
+            if target.index(word) == 0:
+                suffix = target[len(word):]
+                if canConstruct(suffix, wordBank) == True:
+                    return True
+        except ValueError:
+            pass
+    return False
+
+print(canConstruct("abucdef", ["ab", "abc", "cd", "def", "abcd"]))
